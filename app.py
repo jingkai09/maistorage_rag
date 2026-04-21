4289f496406d54e85f3c5f99523bbd2a4a7c0ed5
jingkai09 <126046222+jingkai09@users.noreply.github.com>
2026-04-21 21:08:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # --- 1. CONFIGURATION ---
 # Use your brand-new, private API Key from the new Google Project
-API_KEY = "AIzaSyBcyIXuLWtSGhQnOwBphzUEa8GzGMtM1F8" 
+API_KEY = "AIzaSyDYrFxXwRZ9x9RE5EWgJ71vPAZRM2tRlpc" 
 os.environ["GOOGLE_API_KEY"] = API_KEY
 
 st.set_page_config(page_title="MaiStorage Agentic RAG", layout="wide")
